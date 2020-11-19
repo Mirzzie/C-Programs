fab4868cb2ceb36c006748410e40c855b74d96ac
TheRareKid <mirswad.chikku@gmail.com>
2020-11-19 20:05:36 +0530

    sum of 10 natural nums

---
+++ tensum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sum=0,i;
+    for(i=0;i<=10;i++)
+    {
+        sum = sum+i;
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
