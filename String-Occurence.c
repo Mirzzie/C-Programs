4834f28ef4b05def1eeb6316f096c09482557e99
Mirzzie <mirswad.chikku@gmail.com>
2021-02-22 15:08:26 +0530

    Occurence of a string

---
+++ String-Occurence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+   int i,c=0;
+   char a[50],f;
+   puts("Enter a string:");
+   gets(a);
+   puts("Enter a character:");
+   f=getchar();
+   for(i=0;i<=50;i++)
+   {  
+     if(a[i]==f)
+     c++;
+   }
+   printf("The character %c in string %s occurs %d times ",f,a,c);
+   return 0;
+}
+
+    
