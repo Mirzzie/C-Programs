d77a1a22791a24db827244a1151897d5cf89e8c9
Mirzzie <mirswad.chikku@gmail.com>
2021-02-23 15:17:49 +0530

    star-sandclock

---
+++ star-sandclock.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+   int i,j,n,t;
+   printf("Enter the limit:");
+   scanf("%d",&n);
+   for(i=0;i<n;i++) 
+   { 
+     for(j=n;j>i;j--)
+     {
+        printf("*");
+        i++;
+     }    
+      printf("\n");
+   } 
+
+   for(i=0;i<n;i++) 
+   { 
+     for(j=n;j>i;j--)
+     {
+        printf("*");
+        i++;
+     }    
+      printf("\n");
+   } 
+
+  return 0;
+}
