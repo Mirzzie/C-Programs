8b6c614025d8e911b550fe2e50ad7b8823fde362
TheRareKid <mirswad.chikku@gmail.com>
2021-03-29 21:13:08 +0530

    Symmetric of matrix - check

--- 2D-TRAN-SYMETRIC.c
+++ 2D-TRAN-SYMETRIC.c
@@ -1,45 +1,47 @@
 #include<stdio.h>
 int main()
 {
-    int a[10][10],n,m,i,j,f=0;
-    printf("ENTER YOUR 2D ARRAY SIZE:");
-    scanf("%d%d",&n,&m);
+    int a[10][10],r,c,i,j,f=0;
+    printf("ENTER YOUR MATRIX ORDER:");
+    scanf("%d%d",&r,&c);
     printf("ENTER YOUR ELEMENTS:");
-    for(i=0;i<n;i++)
+    for(i=0;i<r;i++)
     {
-      for(j=0;j<m;j++)
+      for(j=0;j<c;j++)
       scanf("%d",&a[i][j]);
     }
-    printf("PRINTING YOUR ARRAY:\n");
-    for(i=0;i<n;i++)
+    printf("YOUR MATRIX:\n");
+    for(i=0;i<r;i++)
     {
-      for(j=0;j<m;j++)  
+      for(j=0;j<c;j++)
       printf("%d\t",a[i][j]);
       printf("\n");
     }
-    printf("PRINTING YOUR ARRAY TRANSPOSE:\n");
-    for(i=0;i<m;i++)
+    printf("TRANSPOSE OF MATRIX:\n");
+    for(i=0;i<r;i++)
     {
-      for(j=0;j<n;j++)
+      for(j=0;j<c;j++)
         {
            printf("%d\t",a[j][i]);
-           printf("\n");
        }
+       printf("\n");
     }
-     for(i=0;i<m;i++)
-      {
-      for(j=0;j<n;j++)
-      {
-       if(a[i][j]==a[j][i])
-           f=1;
-           break;
-       if(f==1)
-       printf("its SYMMETRIC");
+     for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+       if(a[i][j]!=a[j][i])
+       {
+        printf("YOUR MATRIX IS NOT SYMMETRIC");
+        f=0;
+        break;
+       }
        else
-       printf("Not symmetric");
+       f=1;
+      break;
     }
-
-
+  }
+      if(f==1)
+      printf("YOUR MATRIX IS SYMMETRIC");
     return 0;
-}
 }
