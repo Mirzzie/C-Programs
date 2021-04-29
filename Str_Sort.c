677e13e73de645d32d4f883022c855a7500b06bf
TheRareKid <mirswad.chikku@gmail.com>
2021-04-29 22:03:24 +0530

    String Sorting

---
+++ Str_Sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,count;
+    char str[10][10],temp[10];
+    puts("Number of String:");
+    scanf("%d",&count);
+    puts("Enter strings:");
+    for(i=0;i<=count;i++)
+    gets(str[i]);
+    for(i=0;i<=count;i++)
+    for(j=i+1;j<=count;j++)
+    {
+        if(strcmp(str[i],str[j])>0)
+        {
+            strcpy(temp,str[i]);
+            strcpy(str[i],str[j]);
+            strcpy(str[j],temp);
+        }
+    }
+    printf("Sorted strings:");
+    for(i=0;i<=count;i++)
+    puts(str[i]);
+    return 0;
+
+}
